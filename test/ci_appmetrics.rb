fca22025c45d2364db9c2adf0a629444fe177cfa
Danny Berger <dpb587@gmail.com>
2013-06-18 12:28:11 -0600

    add exception and latency test data /issue #12

--- test/ci_appmetrics.rb
+++ test/ci_appmetrics.rb
@@ -5,7 +5,7 @@ class SimpleCiAppmetricsTest < Test::Unit::TestCase
   def test_respect_for_timestamp
     res = eslog_simple_search('logstash-2013.05.14')
 
-    assert_equal 26, res['hits']['total']
+    assert_equal 44, res['hits']['total']
 
     res = eslog_simple_search('logstash-2013.05.15')
 
@@ -21,7 +21,7 @@ class SimpleCiAppmetricsTest < Test::Unit::TestCase
   def test_search_by_name
     res = eslog_simple_search(
       nil,
-      '@fields.Name=System_CalendarType'
+      '@fields.name:"Latency CIAPI.LogIn"'
     )
 
     assert_equal 1, res['hits']['total']
@@ -30,7 +30,7 @@ class SimpleCiAppmetricsTest < Test::Unit::TestCase
   def test_search_by_value
     res = eslog_simple_search(
       nil,
-      '@fields.value="10.11.12.13"'
+      '@fields.value:"10.11.12.13"'
     )
 
     assert_equal 2, res['hits']['total']
