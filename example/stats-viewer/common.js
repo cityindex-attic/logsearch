2ba8189d2bc2234f38c7460218751b2a8517801a
Danny Berger <dpb587@gmail.com>
2013-07-08 11:14:19 -0600

    simple visual stats viewer for gathered/archived metrics /issue #32

---
+++ example/stats-viewer/common.js
@@ -0,0 +1,215 @@
+var stats = null;
+var ebsVolumes;
+var localMounts;
+var localDisks;
+var localInterfaces;
+var localInterrupts;
+
+document.addEvent(
+    'domready',
+    function () {
+        $('datasource').addEvent(
+            'blur',
+            function () {
+                if (!this.value) {
+                    return;
+                }
+
+                new Request.JSON(
+                    {
+                        url : this.value,
+                        method : 'get',
+                        onSuccess : function (data) {
+                            stats = data;
+                
+                            init();
+                        }
+                    }
+                ).send();
+            }
+        );
+
+        if (window.location.hash.substring(1)) {
+            $('datasource')
+                .set('value', window.location.hash.substring(1))
+                .fireEvent('blur')
+            ;
+        }
+    }
+);
+
+function makeDate(str) {
+    // assumes format of %Y-%m-%dT%TZ
+    return Date.UTC(
+        str.substring(0, 4),
+        str.substring(5, 7),
+        str.substring(8, 10),
+        str.substring(11, 13),
+        str.substring(14, 16),
+        str.substring(17, 19)
+    );
+}
+
+function gatherStats(name) {
+    var parsed = [];
+
+    if (!stats[name]) {
+        alert('Stats named "' + name + '" are not available.');
+
+        return {}
+    }
+
+    if ('aws-' == name.substr(0, 4)) {
+        Array.each(
+            stats[name].Datapoints,
+            function (v) {
+                parsed.push([ makeDate(v.Timestamp), v.Average ]);
+            }
+        );
+    } else {
+        Object.each(
+            stats[name],
+            function (v, k) {
+                parsed.push([ makeDate(k), v ]);
+            }
+        );
+    }
+
+    parsed.sort(
+        function (a, b) {
+            if (a[0] == b[0]) {
+                return 0;
+            }
+
+            return a[0] < b[0] ? -1 : 1;
+        }
+    );
+
+    return parsed;
+}
+
+function createChart(obj) {
+    var chart = new Element(
+        'div',
+        {
+            styles : {
+                height : 300,
+                marginBottom : '20px'
+            }
+        }
+    );
+
+    $('charts').adopt(chart);
+
+    new Highcharts.Chart(
+        Object.merge(
+            {
+                chart : {
+                    type : 'areaspline',
+                    renderTo : chart
+                },
+                plotOptions: {
+                    series: {
+                        animation: false
+                    }
+                },
+                xAxis: {
+                    type: 'datetime'
+                },
+                tooltip: {
+                    formatter: function() {
+                            return '<b>'+ this.series.name +'</b><br/>'+
+                            Highcharts.dateFormat('%e. %b', this.x) +': '+ this.y;
+                    }
+                }
+            },
+            obj
+        )
+    );
+}
+
+function init() {
+    $('charts').empty();
+
+    localMounts = Object.keys(stats)
+        .filter(
+            function (v) {
+                return v.match(/collectd\/df\/df-.*/);
+            }
+        ).map(
+            function (v) {
+                return v.replace(/collectd\/df\/df-([^\/]+)\/.*/, '$1');
+            }
+        ).filter(
+            function (v, i, arr) {
+                return arr.lastIndexOf(v) === i;
+            }
+        )
+    ;
+
+    localDisks = Object.keys(stats)
+        .filter(
+            function (v) {
+                return v.match(/collectd\/disk-.*/);
+            }
+        ).map(
+            function (v) {
+                return v.replace(/collectd\/disk-([^\/]+)\/.*/, '$1');
+            }
+        ).filter(
+            function (v, i, arr) {
+                return arr.lastIndexOf(v) === i;
+            }
+        )
+    ;
+
+    localInterfaces = Object.keys(stats)
+        .filter(
+            function (v) {
+                return v.match(/collectd\/interface\/if_octets-.*/);
+            }
+        ).map(
+            function (v) {
+                return v.replace(/collectd\/interface\/if_octets-([^\/]+)\/.*/, '$1');
+            }
+        ).filter(
+            function (v, i, arr) {
+                return arr.lastIndexOf(v) === i;
+            }
+        )
+    ;
+
+    localInterrupts = Object.keys(stats)
+        .filter(
+            function (v) {
+                return v.match(/collectd\/irq\/irq-.*/);
+            }
+        ).map(
+            function (v) {
+                return v.replace(/collectd\/irq\/irq-([^\/]+)\/.*/, '$1');
+            }
+        ).filter(
+            function (v, i, arr) {
+                return arr.lastIndexOf(v) === i;
+            }
+        )
+    ;
+    
+    ebsVolumes = Object.keys(stats)
+        .filter(
+            function (v) {
+                return v.match(/aws-ebs\/vol-.*/);
+            }
+        ).map(
+            function (v) {
+                return v.replace(/aws-ebs\/vol-([^\/]+)\/.*/, '$1');
+            }
+        ).filter(
+            function (v, i, arr) {
+                return arr.lastIndexOf(v) === i;
+            }
+        )
+    ;
+
+    redraw();
+}
