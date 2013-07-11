cd0b19021f06d4d34bcc18d111ec5626479b7068
David Laing <david@davidlaing.com>
2013-07-11 14:10:24 +0100

    Initial aggregation scripts by @dpb587

---
+++ example/log-event-rate-measurer/process-logs.sh
@@ -0,0 +1,15 @@
+ #!/usr/bin/env bash
+ 
+# this does `rm *`... make sure you're in the proper directory when running
+for SERVICE in AccountInformationGateway AuthenticationGateway CiConnectGateway ClientPreferenceGateway HedgeGateway InstructionProcessorGateway MarketSearchGateway MessageGateway NewsGateway OrderGateway PriceHistoryGateway SimulationGateway TradingApi WatchlistGateway ; do
+    rm -fr *
+
+    for FILE in $(aws s3 list-objects --bucket=ci-elasticsearch-development-flow --prefix="PPE-Logs/" | grep '"Key": "' | sed -r 's/.*: "([^"]+)",.*$/\1/' | grep $SERVICE/) ; do
+        aws s3 get-object --bucket ci-elasticsearch-development-flow --key "$FILE" `echo $FILE | openssl md5 | cut -c10-`.zip > /dev/null
+    done
+
+    unzip -qq -B '*.zip' 1>/dev/null 2>&1
+    rm *.zip
+
+    cat * | $1 | sed -r "s/(.*)/$SERVICE\t\1/"
+done
