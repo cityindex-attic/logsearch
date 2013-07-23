d42864c7e9d2bc8bec245922b167e3ff81c707fc
Danny Berger <dpb587@gmail.com>
2013-07-23 08:33:20 -0600

    Uncomment lines from testing.

--- example/perftest/bin/create-stack.sh
+++ example/perftest/bin/create-stack.sh
@@ -9,12 +9,12 @@ IPADDR=`curl -s ifconfig.me`
 
 echo 'Creating stack...'
 
-#CREATE_STACK=$(aws cloudformation create-stack \
-#    --parameters "[{\"parameter_key\":\"KeyName\",\"parameter_value\":\"$1\"},{\"parameter_key\":\"ClusterName\",\"parameter_value\":\"$STACK_NAME\"},{\"parameter_key\":\"ExternalAccessCidrRange\",\"parameter_value\":\"$IPADDR/32\"}]" \
-#    --stack-name $STACK_NAME \
-#    --tags '{"1":{"name":"cost-centre","value":"elasticsearch-development-flow"}}' \
-#    --template-body "`cat formation.template`"
-#)
+CREATE_STACK=$(aws cloudformation create-stack \
+    --parameters "[{\"parameter_key\":\"KeyName\",\"parameter_value\":\"$1\"},{\"parameter_key\":\"ClusterName\",\"parameter_value\":\"$STACK_NAME\"},{\"parameter_key\":\"ExternalAccessCidrRange\",\"parameter_value\":\"$IPADDR/32\"}]" \
+    --stack-name $STACK_NAME \
+    --tags '{"1":{"name":"cost-centre","value":"elasticsearch-development-flow"}}' \
+    --template-body "`cat formation.template`"
+)
 
 echo 'Waiting for stack creation to complete...'
 
@@ -28,46 +28,43 @@ done
 
 echo 'Waiting a little longer...'
 
-#sleep 300
+sleep 300
 
 echo 'Patching...'
 
-#for NODE in * ; do
-#    if [ -d $NODE ] ; then
-#        echo $NODE
-#        IPADDRESS=$(../../bin/find-instance-ip.sh $NODE)
-#        rsync --progress -auze "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $2" $NODE/ ubuntu@$IPADDRESS:~/perftest-patch
-#
-#        if [ -f $NODE/patch.sh ] ; then
-#            ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $2 ubuntu@$IPADDRESS -t "~/perftest-patch/patch.sh"
-#        fi
-#    fi
-#done
+for NODE in * ; do
+    if [ -d $NODE ] ; then
+        echo $NODE
+        IPADDRESS=$(../../bin/find-instance-ip.sh $NODE)
+        rsync --progress -auze "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $2" $NODE/ ubuntu@$IPADDRESS:~/perftest-patch
+
+        if [ -f $NODE/patch.sh ] ; then
+            ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $2 ubuntu@$IPADDRESS -t "~/perftest-patch/patch.sh"
+        fi
+    fi
+done
 
 echo 'Sleeping to quiet the nodes...'
 
 SESSION_START=`date -u +%Y-%m-%dT%TZ`
 echo "SESSION_START='$SESSION_START'"
 
-#sleep 360
+sleep 360
 
 echo 'Running...'
 
-#./run.sh $1 $2
+./run.sh $1 $2
 
 echo 'Sleeping to quiet the nodes...'
 
-#sleep 360
+sleep 360
 
 SESSION_STOP=`date -u +%Y-%m-%dT%TZ`
 echo "SESSION_STOP='$SESSION_STOP'"
 
 echo 'Gathering stats...'
 
-#sleep 60
-
-SESSION_START='2013-07-17T02:24:59Z'
-SESSION_STOP='2013-07-17T02:57:26Z'
+sleep 60
 
 for NODE in $(aws cloudformation list-stack-resources --stack-name $STACK_NAME | tr ' ' '\n' | grep 'Instance"' | grep -v '"AWS::EC2::Instance",' | sed -E 's/^"(.*)"$/\1/') ; do
     echo $NODE
