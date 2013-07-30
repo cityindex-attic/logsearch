13ccb2b9fe8afa5ad47a2e51c7f57088a5fd76db
Danny Berger <dpb587@gmail.com>
2013-07-30 11:07:31 -0600

    officially switch to 3.0.0-milestone3-pre (87afc4dd50) /issue #64

--- srv/kibana/provision.sh
+++ srv/kibana/provision.sh
@@ -8,7 +8,7 @@ set -e
 #
 
 if [ ! -e $APP_VENDOR_DIR/kibana ] ; then
-    KIBANA_VERSION="050ee74c10851ae9178d471e71d752c5d76986fc"
+    KIBANA_VERSION="87afc4dd50498f69f3ff11d2532b2d26a540e6da"
     echo "Download kibana-dev-$KIBANA_VERSION..."
 
     pushd $APP_VENDOR_DIR/
