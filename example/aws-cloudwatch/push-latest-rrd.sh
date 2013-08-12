c48b00937c94431501fe747074bc17dc50acc9ae
Danny Berger <dpb587@gmail.com>
2013-08-12 13:53:09 -0600

    generalize cloudwatch to push rrdtool lastupdate

---
+++ example/aws-cloudwatch/push-latest-rrd.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# args: cluster_name, rrd_file, metric_name
+
+# * * * * * (. /app/.env ; /app/app/example/aws-cloudwatch/push-latest-rrd.sh logsearch-ppe /var/lib/collectd/rrd/$(/bin/hostname --fqdn)/redis_logstash/gauge-llen_logstash.rrd  QueueSize) >> /app/var/log/cron.log 2>&1
+# * * * * * (. /app/.env ; /app/app/example/aws-cloudwatch/push-latest-rrd.sh logsearch-ppe /var/lib/collectd/rrd/$(/bin/hostname --fqdn)/elasticsearch_logstash/gauge-lag.rrd  IndexLag) >> /app/var/log/cron.log 2>&1
+
+set -e
+
+CLUSTER_NAME=$1
+RRD_FILE=$2
+METRIC_NAME=$3
+
+INSTANCE_ID=$(ec2metadata --instance-id)
+RRD_LINE=$(rrdtool lastupdate $RRD_FILE | tail -n1)
+
+if [[ ! `echo $RRD_LINE | grep -E ": -nan(\$| )"` ]] ; then
+    RRD_DATE=`echo $RRD_LINE | awk -F ':' '{ system("date --date=\"@" $1 "\" +%Y-%m-%dT%H:%M:%SZ") }'`
+    RRD_STAT=$((echo -n 'print eval ' ; echo $RRD_LINE | sed -r "s/.*: ([^ ]+)(\$| ).*/\1/") | perl)
+
+    if [[ ! `echo $RRD_STAT | grep \\\\.` ]] ; then
+        RRD_STAT="$RRD_STAT.0"
+    fi
+
+    ARG1=$(cat <<EOF
+    {
+        "dimensions" : [
+            {
+                "name" : "ClusterName",
+                "value" : "$CLUSTER_NAME"
+            },
+            {
+                "name" : "InstanceId",
+                "value" : "$INSTANCE_ID"
+            }
+        ],
+        "metric_name" : "$METRIC_NAME",
+        "timestamp" : "$RRD_DATE",
+        "unit" : "Count",
+        "value" : $RRD_STAT
+    }
+EOF
+    )
+
+    /usr/local/bin/aws cloudwatch put-metric-data --namespace "logsearch" --metric-data "$ARG1" > /dev/null
+fi
