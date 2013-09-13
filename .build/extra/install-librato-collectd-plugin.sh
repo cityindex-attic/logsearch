b236ab6f0cffab4faf78f76838c581cb0559ed92
Danny Berger <dpb587@gmail.com>
2013-09-13 08:38:47 -0600

    configure collectd with explicit Source /issue #161

--- .build/extra/install-librato-collectd-plugin.sh
+++ .build/extra/install-librato-collectd-plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# args: email, api-token
+# args: email, api-token, optional:source-name
 
 set -e
 
@@ -10,12 +10,19 @@ if [ ! -e /etc/collectd/collectd.d/librato.conf ] ; then
         echo 'Include "/etc/collectd/collectd.d/*.conf"' >> /etc/collectd/collectd.conf
     fi
 
+    if [ "" == "$3" ]; then
+        export SOURCE_NAME="$APP_CLUSTER_NAME.$APP_NODE_NAME.$APP_DEPLOY_NAME.`hostname`"
+    else
+        export SOURCE_NAME="$3"
+    fi
+
     wget -qO /opt/collectd/lib/collectd/plugins/python/collectd-librato.py 'https://raw.github.com/librato/collectd-librato/master/lib/collectd-librato.py'
     cat <<EOF > /etc/collectd/collectd.d/librato.conf
 <Plugin python>
     ModulePath "/opt/collectd/lib/collectd/plugins/python/"
     Import "collectd-librato"
     <Module "collectd-librato">
+        Source "$SOURCE_NAME"
         Email "$1"
         APIToken "$2"
         IncludeRegex "collectd\.cpu-.*,collectd\.df\..*,collectd\.disk-.*,collectd\.elasticsearch\..*,collectd\.entropy\..*,collectd\.interface\..*,collectd\.load\..*,collectd\.memory\..*,collectd\.processes\..*,collectd\.redis_.*,collectd\.users\..*"
