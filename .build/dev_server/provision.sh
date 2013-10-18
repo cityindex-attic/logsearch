8a63e77454448fab0a7d4312ac7f9e5532eb3f55
Danny Berger <dpb587@gmail.com>
2013-10-18 10:31:56 -0500

    Might as well make redis listen on 0.0.0.0, too.

--- .build/dev_server/provision.sh
+++ .build/dev_server/provision.sh
@@ -35,8 +35,8 @@ echo '$APP_USER hard nofile 64000' >> /etc/security/limits.d/$APP_USER.conf
 
 /app/app/bin/default-app-dir > /app/.env
 echo "export APP_USER='$APP_USER'" >> /app/.env
-echo "export APP_CONFIG_ES_IPADDRESS='0.0.0.0'" >> /app/.env
-echo 'export APP_CONFIG_REDIS_IPADDRESS="127.0.0.1"' >> /app/.env
+echo 'export APP_CONFIG_ES_IPADDRESS=0.0.0.0' >> /app/.env
+echo 'export APP_CONFIG_REDIS_IPADDRESS=0.0.0.0' >> /app/.env
 echo 'export APP_CONFIG_REDIS_KEY=logstash' >> /app/.env
 echo 'export APP_CONFIG_IMPORTQUEUE_KEY=importqueue' >> /app/.env
 chmod +x /app/.env
