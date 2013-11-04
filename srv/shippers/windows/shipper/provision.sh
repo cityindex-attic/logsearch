3e9c7d7c655e2a29fef18d02577182782a0e657d
David Laing <david.laing@cityindex.com>
2013-11-04 09:50:13 +0000

    Use unpatched logstash-1.2.2-flatjar.jar

--- srv/shippers/windows/shipper/provision.sh
+++ srv/shippers/windows/shipper/provision.sh
@@ -11,9 +11,9 @@ function display_file_info {
 #
 
 if [ ! -e $DEST/logstash.jar ] ; then
-    echo "Downloading logstash-1.2.1-monolithic-ci-b1.jar(patched)..."
+    echo "Downloading logstash-1.2.2-flatjar.jar..."
     
-    curl --location -o $DEST/logstash.jar http://ci-elasticsearch-development-flow.s3.amazonaws.com/logstash-1.2.1-monolithic-ci-b1.jar
+    curl --location -o $DEST/logstash.jar https://download.elasticsearch.org/logstash/logstash/logstash-1.2.2-flatjar.jar
 fi
 
 echo "logstash:$(display_file_info $DEST/logstash.jar)"
