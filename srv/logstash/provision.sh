0148b896538217b778b97bae556478b77a4871e7
Danny Berger <dpb587@gmail.com>
2013-11-08 15:37:21 +0000

    switch to logstash 1.2.2 /issue #220

--- srv/logstash/provision.sh
+++ srv/logstash/provision.sh
@@ -8,9 +8,9 @@ set -e
 #
 
 if [ ! -e $APP_VENDOR_DIR/logstash.jar ] ; then
-    echo "Downloading logstash-1.2.1..."
+    echo "Downloading logstash-1.2.2..."
 
-    curl --location -o $APP_VENDOR_DIR/logstash.jar https://logstash.objects.dreamhost.com/release/logstash-1.2.1-flatjar.jar
+    curl --location -o $APP_VENDOR_DIR/logstash.jar https://download.elasticsearch.org/logstash/logstash/logstash-1.2.2-flatjar.jar
 fi
 
-echo "logstash:$(java -jar $APP_VENDOR_DIR/logstash.jar -v | awk -F ' ' '/logstash/ { print $2 }')"
+echo "logstash:$(java -jar $APP_VENDOR_DIR/logstash.jar version | awk -F ' ' '/logstash/ { print $2 }')"
