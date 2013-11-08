7b56edc54e431675d04243834f720c7722e08665
David Laing <david@davidlaing.com>
2013-11-08 17:06:58 +0000

    Extract floats from value field into duration field (of type Float)

--- srv/logstash/test/ci_appmetrics.rb
+++ srv/logstash/test/ci_appmetrics.rb
@@ -1,24 +1,27 @@
 require_relative 'common'
-require "test/unit"
+require 'minitest/autorun'
 
-class SimpleCiAppmetricsTest < Test::Unit::TestCase
-  def test_respect_for_timestamp
+class CI_AppMetrics; end;
+
+describe CI_AppMetrics, "type=ci_appmetrics" do
+
+  it "should put events in the right index for their timestamp" do
     res = eslog_simple_search('logstash-2013.05.14')
 
-    assert_equal 44, res['hits']['total']
+    assert_equal 46, res['hits']['total']
 
     res = eslog_simple_search('logstash-2013.05.15')
 
     assert_equal 1, res['hits']['total']
   end
 
-  def test_no_events_inferred_today
-    assert_raise RuntimeError do
+  it "should not infer any events for today" do
+    assert_raises(RuntimeError) do
       eslog_simple_search("logstash-#{Time.new.strftime('%Y-%m-%d')}")
     end
   end
 
-  def test_search_by_name
+  it "should be possible to search by name" do
     res = eslog_simple_search(
       nil,
       'name:"Latency CIAPI.LogIn"'
@@ -27,7 +30,7 @@ class SimpleCiAppmetricsTest < Test::Unit::TestCase
     assert_equal 1, res['hits']['total']
   end
 
-  def test_search_by_value
+  it "should be possible to search by value" do
     res = eslog_simple_search(
       nil,
       'value:"10.11.12.13"'
@@ -36,22 +39,14 @@ class SimpleCiAppmetricsTest < Test::Unit::TestCase
     assert_equal 2, res['hits']['total']
   end
 
-  def test_latency_values_set_to_type_float
+  it "should extract all numeric values into duration" do
     res = eslog_simple_search(
       nil,
-      '@message:"Latency CIAPI.*" OR "Latency General.*"'
+      '_exists_:duration'
     )
 
-    assert_equal 1, res['hits']['total']
-    assert_equal "2013-09-03T01:04:54.258Z", res['hits']['hits'][0]['_source']['@timestamp']
+    assert_equal 25, res['hits']['total']
+    res['hits']['hits'][0]['_source']['duration'].must_be_instance_of Float
   end
 
-  def test_non_latency_values_set_to_type_string
-    res = eslog_simple_search(
-      nil,
-      'NOT @message:"Latency *"'
-    )
-
-    assert_equal 1, res['hits']['total']
-  end
-end
+end #describe
