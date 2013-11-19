626e3621bed2d1a665f1f66172172e1de3e0460a
Danny Berger <dpb587@gmail.com>
2013-11-19 07:01:19 -0700

    switch tests to run through redis for now /issue #223

---
+++ srv/logstash/test/do-import-redis.rb
@@ -0,0 +1,45 @@
+require_relative 'common'
+
+#
+# import the test data
+#
+
+system "cd #{File.dirname(__FILE__)}/../../../ && rake logstash:pv_to_redis[#{ARGV[0]},#{ARGV[1]}]"
+
+raise "Failed to import '#{ARGV[1]}' as #{ARGV[0]}" if 0 < $?.exitstatus
+
+
+# logstash workers have a slight delay with queueing/flushing
+sleep 8
+
+#
+# make sure everything parsed okay
+#
+
+res = eslog_search(
+  "_search",
+  {
+    "query" => {
+      "filtered" => {
+        "query" => {
+          "query_string" => {
+            "query" => "@tags:\"_grokparsefailure\""
+          }
+        }
+      }
+    },
+    "size" => 10,
+    "sort" => [
+      {
+        "@timestamp" => {
+          "order" => "desc"
+        }
+      }
+    ]
+  }
+)
+
+if (0 < res['hits']['total'])
+    raise "Some log events were not parsed correctly (#{res['hits']['total']} events) " +
+          "- the most recent 10 are shown: #{JSON.pretty_generate(res)}"
+end
