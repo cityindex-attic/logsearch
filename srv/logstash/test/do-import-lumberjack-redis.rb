ffedf0ff99033886ed9dc9d7b3e80ba980b30816
David Laing <david@davidlaing.com>
2013-11-29 01:24:10 +0000

    Cleanup do-impotr-lumberjack

--- srv/logstash/test/do-import-lumberjack-redis.rb
+++ srv/logstash/test/do-import-lumberjack-redis.rb
@@ -4,37 +4,39 @@ require_relative 'common'
 # import path is:  file -> lumberjack shipper -> lumberjack endpoint -> redis -> parser -> elasticsearch
 #
 
+puts "---> Restarting app-logstash_redis"
+puts `sudo service app-logstash_redis restart`
+
 def ensure_service_running(service_name)
-  puts "Ensuring #{service_name} is running..."
+  puts "  Ensuring #{service_name} is running..."
   unless /start\/running/ =~ `service #{service_name} status` 
     raise "service #{service_name} must be running.\nRun 'sudo start app' before running this test"
   end
 end
 
-#ensure required services are running
+puts "---> Ensuring other required services are running"
 ensure_service_running "app-elasticsearch"
 ensure_service_running "app-redis"
 ensure_service_running "app-lumberjack_redis"
-ensure_service_running "app-logstash_redis"
 
-# ensure lumberjack and lumberjack keys are installed
-# system "cd #{File.dirname(__FILE__)}/../../../ && rake lumberjack:provision"
-# system "cd #{File.dirname(__FILE__)}/../../../ && rake lumberjack:generate_keys"
+puts "---> Ensuring lumberjack and lumberjack keys are installed"
+unless File.exists? "#{ENV['APP_VENDOR_DIR']}/lumberjack"
+  system "cd #{File.dirname(__FILE__)}/../../../ && rake lumberjack:provision"
+end
+unless File.exists? "#{ENV['APP_DATA_DIR']}/lumberjack.key"
+  system "cd #{File.dirname(__FILE__)}/../../../ && rake lumberjack:generate_keys"
+  puts `sudo service app-lumberjack_redis restart`
+  puts "Restarting app-lumberjack_redis to ensure its running the latest config file..."
+  sleep 10
+end
 
-# # Make sure the app-logstash_redis is running the latest config file
-# puts "Waiting for app-lumberjack_redis to be ready..."
-# puts `sudo service app-lumberjack_redis stop`
-# sleep 2
-# puts `sudo service app-lumberjack_redis start`
-# system "while ! nc -vz 127.0.0.1 5043 2>/dev/null ; do echo -n . & sleep 2 ; done"
-
-puts "\nShipping the test log file via lumberjack shipper -> lumberjack endpoint -> redis"
+puts "---> Shipping the test log file via lumberjack shipper -> lumberjack endpoint -> redis"
 system "cd #{File.dirname(__FILE__)}/../../../ && rake lumberjack:ship_to_lumberjack_endpoint[#{ARGV[1]}]"
 
 raise "Failed to import #{ARGV[1]} using lumberjack" if 0 < $?.exitstatus
 
-# logstash workers have a slight delay with queueing/flushing
-sleep 15
+puts "---> Waiting for logstash-redis"
+sleep 30
 
 #
 # make sure everything parsed okay
@@ -67,4 +69,3 @@ if (0 < res['hits']['total'])
     raise "Some log events were not parsed correctly (#{res['hits']['total']} events) " +
           "- the most recent 10 are shown: #{JSON.pretty_generate(res)}"
 end
-
