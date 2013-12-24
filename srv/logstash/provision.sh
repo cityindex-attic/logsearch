0cb03aa3464b6abb37a20b9a4c8c27ac226f09f7
Danny Berger <dpb587@gmail.com>
2013-12-23 23:54:47 -0700

    drop logstash version reporting from provisioning since init time is slow

--- srv/logstash/provision.sh
+++ srv/logstash/provision.sh
@@ -13,5 +13,3 @@ if [ ! -e $APP_VENDOR_DIR/logstash.jar ] ; then
 
     curl --location -o $APP_VENDOR_DIR/logstash.jar https://download.elasticsearch.org/logstash/logstash/logstash-1.2.2-flatjar.jar
 fi
-
-echo "logstash:$(java -jar $APP_VENDOR_DIR/logstash.jar version | awk -F ' ' '/logstash/ { print $2 }')"
