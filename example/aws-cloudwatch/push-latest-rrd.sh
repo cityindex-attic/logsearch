7fd920a285a9b3783f30b6be848d83daae710063
Danny Berger <dpb587@gmail.com>
2014-01-23 07:30:06 -0700

    change push-latest-rrd to match env/service/role style; /issue #317

--- example/aws-cloudwatch/push-latest-rrd.sh
+++ example/aws-cloudwatch/push-latest-rrd.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# args: cluster_name, rrd_file, metric_name
+# args: rrd_file, environment_name, service_name, metric_name
 
-# * * * * * (. /app/.env ; /app/app/example/aws-cloudwatch/push-latest-rrd.sh logsearch-ppe /var/lib/collectd/rrd/$(/bin/hostname --fqdn)/redis_logstash/gauge-llen_logstash.rrd  QueueSize) >> /app/var/log/cron.log 2>&1
-# * * * * * (. /app/.env ; /app/app/example/aws-cloudwatch/push-latest-rrd.sh logsearch-ppe /var/lib/collectd/rrd/$(/bin/hostname --fqdn)/elasticsearch_logstash/gauge-lag.rrd  IndexLag) >> /app/var/log/cron.log 2>&1
+# * * * * * (. /app/.env ; /app/app/example/aws-cloudwatch/push-latest-rrd.sh dev logsearch-repodev QueueSize /var/lib/collectd/rrd/$(/bin/hostname --fqdn)/redis_logstash/gauge-llen_logstash.rrd) >> /app/var/log/cron.log 2>&1
+# * * * * * (. /app/.env ; /app/app/example/aws-cloudwatch/push-latest-rrd.sh dev logsearch-repodev IndexLag /var/lib/collectd/rrd/$(/bin/hostname --fqdn)/elasticsearch_logstash/gauge-lag.rrd) >> /app/var/log/cron.log 2>&1
 
 set -e
 
-CLUSTER_NAME=$1
-RRD_FILE=$2
-METRIC_NAME=$3
+RRD_FILE=$1
+ENVIRONMENT_NAME=$2
+SERVICE_NAME=$3
+ROLE_NAME=$4
 
 INSTANCE_ID=$(ec2metadata --instance-id)
 RRD_LINE=$(rrdtool lastupdate $RRD_FILE | tail -n1)
@@ -26,11 +27,11 @@ if [[ ! `echo $RRD_LINE | grep -E ": -nan(\$| )"` ]] ; then
     {
         "Dimensions" : [
             {
-                "Name" : "ClusterName",
-                "Value" : "$CLUSTER_NAME"
+                "Name" : "Service",
+                "Value" : "$SERVICE_NAME"
             }
         ],
-        "MetricName" : "$METRIC_NAME",
+        "MetricName" : "$ROLE_NAME",
         "Timestamp" : "$RRD_DATE",
         "Unit" : "Count",
         "Value" : $RRD_STAT
@@ -38,5 +39,5 @@ if [[ ! `echo $RRD_LINE | grep -E ": -nan(\$| )"` ]] ; then
 EOF
     )
 
-    /usr/local/bin/aws cloudwatch put-metric-data --namespace "logsearch" --metric-data "$ARG1" > /dev/null
+    /usr/local/bin/aws cloudwatch put-metric-data --namespace "$ENVIRONMENT_NAME" --metric-data "$ARG1" > /dev/null
 fi
